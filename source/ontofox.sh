ac7ba81d9c0c114ab34448fac7e0d934a203961c
Peter Midford <peter.midford@gmail.com>
2012-03-27 23:46:03 -0400

    update generation script and SPD and NBO inputs

--- source/ontofox.sh
+++ source/ontofox.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Extracting IAO"
-curl -s -F file=@ontofoxIAOinput.txt -o ../ontologies/IAO_input_test.owl http://ontofox.hegroup.org/service.php
+curl -s -F file=@ontofoxIAOinput.txt -o ../ontologies/IAO_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting NCBI taxonomy"
-curl -s -F file=@ontofoxNCBIinput.txt -o ../ontologies/NCBI_input_test.owl http://ontofox.hegroup.org/service.php
+curl -s -F file=@ontofoxNCBIinput.txt -o ../ontologies/NCBI_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting NBO"
-curl -s -F file=@ontofoxNBOinput.txt -o ../ontologies/NBO_input_test.owl http://ontofox.hegroup.org/service.php
+curl -s -F file=@ontofoxNBOinput.txt -o ../ontologies/NBO_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting SPD"
-curl -s -F file=@ontofoxSPDinput.txt -o ../ontologies/SPD_input_test.owl http://ontofox.hegroup.org/service.php
+curl -s -F file=@ontofoxSPDinput.txt -o ../ontologies/SPD_import.owl http://ontofox.hegroup.org/service.php
 
 
 
