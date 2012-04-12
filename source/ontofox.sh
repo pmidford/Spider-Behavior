4c17cac59317b9f93169bd20ba33c8a54bfd1a29
Peter Midford <peter.midford@gmail.com>
2012-04-12 00:01:06 -0400

    made use of imported has_participant relation, also adding missing updates to ontofox script and the input file for RO.

--- source/ontofox.sh
+++ source/ontofox.sh
@@ -3,6 +3,8 @@ echo "Extracting IAO"
 curl -s -F file=@ontofoxIAOinput.txt -o ../ontologies/IAO_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting NCBI taxonomy"
 curl -s -F file=@ontofoxNCBIinput.txt -o ../ontologies/NCBI_import.owl http://ontofox.hegroup.org/service.php
+echo "Extracting RO"
+curl -s -F file=@ontofoxROinput.txt -o ../ontologies/RO_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting NBO"
 curl -s -F file=@ontofoxNBOinput.txt -o ../ontologies/NBO_import.owl http://ontofox.hegroup.org/service.php
 echo "Extracting SPD"
