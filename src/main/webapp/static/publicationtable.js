e82b86e5b4ae1ebf32a8e594671512010a1815a1
Peter Midford <peter.midford@gmail.com>
2013-10-04 13:03:47 -0400

    integrate bootstrap library

--- src/main/webapp/static/publicationtable.js
+++ src/main/webapp/static/publicationtable.js
@@ -1,19 +1,18 @@
-var publicationtable = function (resultobj){
+var publicationtable = function (resultobj) {
+    "use strict";
     if (resultobj.results) {
-        var result = "";
-        result = result + "<table border='1'>\n";
+        var result, i;
+        result = "<table border='1'>\n";
         result = result + "  <tr>\n";
         result = result + "  <th>" + resultobj.head.vars[0] + "</th>\n";
         result = result + "  </tr>\n";
-        for(i=0;i < resultobj.results.bindings.length; i += 1){
-	    result = result + "  <tr>\n";
-	    result = result + "    <td>" + resultobj.results.bindings[i].publication.value + "</td>\n";
-           result = result + "  </tr>\n"
-	       }
+        for (i = 0; i < resultobj.results.bindings.length; i += 1) {
+            result = result + "  <tr>\n";
+            result = result + "    <td>" + resultobj.results.bindings[i].publication.value + "</td>\n";
+            result = result + "  </tr>\n";
+        }
         result = result + "</table>";
         return result;
     }
-    else {
-        return "<p>No results</p>";
-    }
-}
+    return "<p>No results</p>";
+};
