2dc7747e196dcd621a2fe562c12960362349f4be
Peter Midford <peter.midford@gmail.com>
2014-03-02 21:30:45 -0500

    implement uri links to text in previous column

--- src/main/webapp/static/resulttable.js
+++ src/main/webapp/static/resulttable.js
@@ -1,25 +1,51 @@
+var uricolumn = function (resultobj, column) {
+    "use strict";
+    var i, binding, celltype;
+    for (i = 0; i < resultobj.results.bindings.length; i += 1) {
+        binding = resultobj.results.bindings[i];
+        celltype = binding[column].type;
+        if (celltype !== 'uri') {
+            return false;
+        }
+    }
+    return true;
+};
+
 var resulttable = function (resultobj) {
     "use strict";
     if (resultobj === 'no results') {
         return "<p>No results</p>";
     }
     if (resultobj.results.bindings && resultobj.results.bindings.length > 0) {
-        var result, i, j, binding, fieldtype;
+        var result, i, j, binding, fieldtype, fieldname, nextfieldname, fielddatatype, nextfielddatatype;
         result = "<table class='table'>\n";
         result = result + "  <tr>\n";
         for (i = 0; i < resultobj.head.vars.length; i += 1) {
             fieldtype = resultobj.head.vars[i];
-            result = result + "  <th>" + fieldtype + "</th>\n";
+            if (!uricolumn(resultobj, fieldtype)) {
+                result = result + "  <th>" + fieldtype + "</th>\n";
+            }
         }
         result = result + "  </tr>\n";
         for (j = 0; j < resultobj.results.bindings.length; j += 1) {
             binding = resultobj.results.bindings[j];
             result = result + "  <tr>\n";
             for (i = 0; i < resultobj.head.vars.length; i += 1) {
-                fieldtype = resultobj.head.vars[i];
-                //may want to call decodeURIComponent somewhere around here
-                //fielddatatype = binding[fieldtype].type;
-                result = result + "    <td>" + binding[fieldtype].value + "</td>\n";
+                fieldname = resultobj.head.vars[i];
+                fielddatatype = binding[fieldname].type;
+                if (i < resultobj.head.vars.length - 1) {
+                    nextfielddatatype = binding[resultobj.head.vars[i + 1]].type;
+                } else {
+                    nextfielddatatype = '';
+                }
+                if (nextfielddatatype === 'uri') {
+                    nextfieldname = resultobj.head.vars[i + 1];
+                    result = result + "    <td> <a href= '" + binding[nextfieldname].value + "'>" + binding[fieldname].value + "</a></td>\n";
+                } else {
+                    if (fielddatatype !== 'uri') {
+                        result = result + "    <td>" + binding[fieldname].value + "</td>\n";
+                    }
+                }
             }
             result = result + "  </tr>\n";
         }
@@ -28,3 +54,4 @@ var resulttable = function (resultobj) {
     }
     return "<p>No results</p>";
 };
+
