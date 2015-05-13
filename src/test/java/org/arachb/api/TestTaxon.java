54c9d5a001f1d3ee5adf926d81c1d9a828ea1a76
Peter Midford <peter.midford@gmail.com>
2015-05-12 20:55:56 -0400

    add @Before and @Test annotations to methods as appropriate

--- src/test/java/org/arachb/api/TestTaxon.java
+++ src/test/java/org/arachb/api/TestTaxon.java
@@ -1,22 +1,28 @@
 package org.arachb.api;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class TestTaxon extends TestCase {
 	
 	final static String TAXONNCBIID = "http://purl.obolibrary.org/obo/NCBITaxon_336608";
 
+	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
 
+	@Test
 	public void testGetName2EthogramQuery() {
 		Taxon testTaxon = new Taxon();
 		String testQuery = testTaxon.getName2EthogramQuery("Habronattus");
-		//System.out.println(testQuery);
+		System.out.println(testQuery);
 		assert(testQuery.contains("Habronattus"));
 	}
 	
+	@Test
 	public void testGetName2TaxonIdQuery(){
 		Taxon testTaxon = new Taxon();
 		String testQuery = testTaxon.getName2TaxonIdQuery("Habronattus");
