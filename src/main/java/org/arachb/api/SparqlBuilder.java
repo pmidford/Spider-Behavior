06280fd451ddcfa684c27e8be2a8b600f8ed3004
Peter Midford <peter.midford@gmail.com>
2015-08-24 07:17:28 -0400

    added 1 argument version of addClause, CLAUSEINDENT spaces constant and debug method which dumps state of builder to console

--- src/main/java/org/arachb/api/SparqlBuilder.java
+++ src/main/java/org/arachb/api/SparqlBuilder.java
@@ -1,32 +1,56 @@
 package org.arachb.api;
 
 public class SparqlBuilder {
-	
+
 	private final StringBuilder b = new StringBuilder(500);
-	
+
 	private SparqlBuilder(){
-		
+
 	}
 
 	public static SparqlBuilder startSparql(){
 		return new SparqlBuilder();
 	}
-	
+
+	public static SparqlBuilder startSparqlWithOBO(){
+		SparqlBuilder b = new SparqlBuilder();
+		b.addText(Util.OBOPREFIX, true);
+		b.addText("%n",true);
+		return b;
+	}
+
 	public void addText(String text, boolean engage){
 		if (engage){
 			b.append(String.format(text));
 		}
 	}
-	
+
+	public void addText(String text){
+		b.append(String.format(text));
+	}
+
+
+	final static String CLAUSEINDENT = "       ";
 	public void addClause(String clauseBase, boolean engage){
 		if (engage){
-			b.append("       ");
+			b.append(CLAUSEINDENT);
 			b.append(clauseBase);
 			b.append(String.format(". %n"));
 		}
 	}
-	
-	
+
+	public void addClause(String clauseBase){
+		b.append(CLAUSEINDENT);
+		b.append(clauseBase);
+		b.append(String.format(". %n"));
+	}
+
+
+	public void debug(){
+		System.out.println("SparqlBuilder buffer contains " + b.toString());
+	}
+
+
 	public String finish(){
 		return b.toString();
 	}
