cd0b1ca6e785a1955d28f64141dd71639ed60c6a
Peter Midford <peter.midford@gmail.com>
2015-09-04 10:57:19 -0400

    worth saving, need to generalize

---
+++ forceupdate.sh
@@ -0,0 +1,4 @@
+export SESAME_REPOS="${HOME}/.aduna/repositories"
+rm ./src/main/resources/arachb.owl
+cp ../arachtools/owlbuilder/test.owl ./src/main/resources/arachb.owl
+rm -rf $SESAME_REPOS/test1
