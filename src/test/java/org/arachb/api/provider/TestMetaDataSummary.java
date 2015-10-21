f8fdbb311091760c8594efdda5969c7cd925fcaa
Peter Midford <peter.midford@gmail.com>
2015-10-20 20:27:41 -0400

    add unit tests for page support classes

---
+++ src/test/java/org/arachb/api/provider/TestMetaDataSummary.java
@@ -0,0 +1,124 @@
+package org.arachb.api.provider;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class TestMetaDataSummary {
+
+	
+	//ugly, but apparently necessary
+	enum Field implements PageField{
+		FIELD1("field1","field1",""),
+		FIELD2("field2","field2","");
+		
+		private final String key;
+		private final String displayStr;
+		private final String separator;
+		static final Map<String,Field> keylookup = new HashMap<>();
+		
+		Field(String k,String ds,String s){
+			this.key = k;
+			this.displayStr = ds;
+			this.separator = s;
+		}
+		
+		@Override
+		public String displayString(){
+			return displayStr;
+		}
+		
+		@Override
+		public String formatValue(String fieldValue){
+			return fieldValue;
+		}
+
+		@Override
+		public String keyString() {
+			return key;
+		}
+
+		@Override
+		public PageField lookupKey(String key) {
+			return Field.keylookup.get(key);
+		}
+	}
+
+	
+	
+	final static String dummyJson = "{\"field1\": \"Habronattus\" , \"field2\": 2}";
+	
+	static private MetaDataSummary mds1; 
+	private MetaDataSummary mds2;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		mds1 = new MetaDataSummary(dummyJson);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		mds2 = new MetaDataSummary();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mds2 = null;
+	}
+
+	@Test
+	public void testMetaDataSummary() {
+		MetaDataSummary mds = new MetaDataSummary();
+		assertThat(mds.raw(),equalTo(null));
+	}
+
+	@Test
+	public void testMetaDataSummaryString() throws IOException {
+		assertThat(mds1.raw(),equalTo(dummyJson));
+	}
+
+	@Test
+	public void testRaw() throws IOException {
+		assertThat(mds1.raw(),equalTo(dummyJson));
+	}
+
+	@Test
+	public void testContains() {
+		assert(mds1.contains("field1"));
+		assert(mds1.contains("field2"));
+		assertFalse(mds1.contains("bad field"));
+	}
+
+	@Test
+	public void testAdd() {
+		mds2.add("testfield", "testvalue");
+		assert(mds2.contains("testfield"));
+		assertThat(mds2.get("testfield"),equalTo("testvalue"));
+		assertFalse(mds2.contains("testfield2"));
+	}
+
+	@Test
+	public void testGet() {
+		assertThat(mds1.get("field1"),equalTo("Habronattus"));
+		assertThat(mds1.get("field2"),equalTo("2"));
+		assertThat(mds1.get("field3"),equalTo(null));
+	}
+
+	@Test
+	public void testGenerateResults() {
+		String fieldList = mds1.generateResults(Field.values());
+		assert(fieldList.startsWith("<ul>"));
+		assert(fieldList.endsWith("</ul>"));
+	}
+
+}
---
+++ src/test/java/org/arachb/api/provider/TestNarrativePage.java
@@ -0,0 +1,46 @@
+package org.arachb.api.provider;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestNarrativePage {
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNarrativePage() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGenerateHTML() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testNarrativeLabelQuery() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testNarrativeEventsQuery() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	
+}
