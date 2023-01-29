8737f90c4b902bc887798f36f0b2eb302214a350
midford <peter.midford@gmail.com>
2023-01-29 14:21:14 -0500

    update variable naming and scope; add local style instead of deprecated border attribute for table

--- src/main/webapp/static/publicationtable.js
+++ src/main/webapp/static/publicationtable.js
@@ -1,14 +1,13 @@
-var publicationtable = function (resultobj) {
+const publicationtable = function (resultobj) {
     "use strict";
     if (resultobj && resultobj !== 'no results') {
-        var result, i, binding;
-        var fieldtype = resultobj.head.vars[0]
-        result = "<table border='1'>\n";
+        const fieldtype = resultobj.head.vars[0]
+        let result = "<table style=\"border-width: 1px;\">\n";
         result = result + "  <tr>\n";
         result = result + "  <th>" + fieldtype + "</th>\n";
         result = result + "  </tr>\n";
-        for (i = 0; i < resultobj.results.bindings.length; i += 1) {
-            binding = resultobj.results.bindings[i];
+        for (let i = 0; i < resultobj.results.bindings.length; i += 1) {
+            let binding = resultobj.results.bindings[i];
             result = result + "  <tr>\n";
             result = result + "    <td>" + binding[fieldtype].value + "</td>\n";
             result = result + "  </tr>\n";
