fed718e6f1226d8124dd9f67c1ca68e0550a3fb9
midford <peter.midford@gmail.com>
2023-01-29 14:53:13 -0500

    IDE suggested change in visibility.

--- src/main/java/org/arachb/api/provider/ArachbPage.java
+++ src/main/java/org/arachb/api/provider/ArachbPage.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public interface ArachbPage {
 	
-	public String generateHTML() throws IOException;
+	String generateHTML() throws IOException;
 	
 	public String generatejson() throws IOException;
 
