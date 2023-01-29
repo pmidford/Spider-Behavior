0b4bdaa231d50b52fbe0016be123bc5d9c2efffb
midford <peter.midford@gmail.com>
2023-01-29 14:40:25 -0500

    Just use a css class so this looks like the other tables.  Also fix the naming - IDE seems to prefer camelcase for js variables.

--- src/main/webapp/static/publicationtable.js
+++ src/main/webapp/static/publicationtable.js
@@ -1,15 +1,15 @@
-const publicationtable = function (resultobj) {
+const publicationtable = function (resultObj) {
     "use strict";
-    if (resultobj && resultobj !== 'no results') {
-        const fieldtype = resultobj.head.vars[0]
-        let result = "<table style=\"border-width: 1px;\">\n";
+    if (resultObj && resultObj !== 'no results') {
+        const fieldType = resultObj.head.vars[0]
+        let result = "<table class='table'>\n";
         result = result + "  <tr>\n";
-        result = result + "  <th>" + fieldtype + "</th>\n";
+        result = result + "  <th>" + fieldType + "</th>\n";
         result = result + "  </tr>\n";
-        for (let i = 0; i < resultobj.results.bindings.length; i += 1) {
-            let binding = resultobj.results.bindings[i];
+        for (let i = 0; i < resultObj.results.bindings.length; i += 1) {
+            let binding = resultObj.results.bindings[i];
             result = result + "  <tr>\n";
-            result = result + "    <td>" + binding[fieldtype].value + "</td>\n";
+            result = result + "    <td>" + binding[fieldType].value + "</td>\n";
             result = result + "  </tr>\n";
         }
         result = result + "</table>";
