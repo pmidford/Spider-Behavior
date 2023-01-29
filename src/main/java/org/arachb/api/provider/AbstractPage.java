56a0dcd8fab76e61b972d6f651ba8efb4ea8b36d
midford <peter.midford@gmail.com>
2023-01-29 14:52:32 -0500

    Formatting and IDE suggested fixes.

--- src/main/java/org/arachb/api/provider/AbstractPage.java
+++ src/main/java/org/arachb/api/provider/AbstractPage.java
@@ -5,11 +5,11 @@ import java.util.Date;
 
 public abstract class AbstractPage implements ArachbPage {
 
-	final static String JQUERYSTRING = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
+	final static String JQUERYSTRING = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
 	final static String BOOTSTRAPSTRING = "../static/bootstrap/js/bootstrap.min.js";
 	final static String ENVSTRING = "../static/env.js";
 	
-	MetaDataSummary metadata;
+	final MetaDataSummary  metadata;
 	
 	protected AbstractPage(MetaDataSummary mds){
 		metadata = mds;
@@ -21,7 +21,7 @@ public abstract class AbstractPage implements ArachbPage {
 		result += "   <meta charset=\"utf-8\"/>" + ls;
 		result += "   <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" + ls;
 		result += "   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>" +ls;
-		String titleTemplate = "   <title>%s</title>%n";
+		String titleTemplate = "   <title>%s</title>\n";
 		if (metadata.contains("title")){
 			result += String.format(titleTemplate,metadata.get("title"));
 		}
@@ -78,7 +78,7 @@ public abstract class AbstractPage implements ArachbPage {
 	
 	String addScript(String script_url){
 		String result = "   <script type=\"text/javascript\" ";
-		result += String.format("           src=\"%s\"> %n", script_url);
+		result += String.format("           src=\"%s\"> \n", script_url);
 		result += "   </script>" + System.lineSeparator();
 		return result;
 	}
@@ -100,7 +100,7 @@ public abstract class AbstractPage implements ArachbPage {
 		result += "        url: myhost + \"taxon\" + $(location).attr('search')," + ls;
 		result += "        dataType: \"text\"" + ls;
 		result += "      }" + ls;
-		String donefunc = String.format("      ).done(function(data, testStatus, jqXHR){$(\"%s\").html(resulttable(eval( '(' + data + ')')))})%n",target);
+		String donefunc = String.format("      ).done(function(data, testStatus, jqXHR){$(\"%s\").html(resulttable(JSON.parse(data)))})\n",target);
 		result += donefunc;
 		result += "      .fail(function(jqXHR, textStatus, errorThrown) { alert(\"error: \" + textStatus); });" + ls;
 		result += "     });" + ls;
