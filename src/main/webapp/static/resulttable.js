a612047c72e7c2b97a4d3fc0eaa4305f7165c851
midford <peter.midford@gmail.com>
2023-01-29 14:24:23 -0500

    Fix some out-of-date js (e.g., use of var) and add some more scoped variables.

--- src/main/webapp/static/resulttable.js
+++ src/main/webapp/static/resulttable.js
@@ -1,45 +1,52 @@
-var uricolumn = function (resultobj, column) {
-    "use strict";
-    var i, binding, celltype;
-    for (i = 0; i < resultobj.results.bindings.length; i += 1) {
-        binding = resultobj.results.bindings[i];
-        celltype = binding[column].type;
+"use strict";
+function uricolumn(resultobj, column) {
+    for (let i = 0; i < resultobj.results.bindings.length; i += 1) {
+        let binding = resultobj.results.bindings[i];
+        let celltype = binding[column].type;
         if (celltype !== 'uri') {
             return false;
         }
     }
     return true;
-};
+}
 
-var resulttable = function (resultobj) {
-    "use strict";
+function resulttable(resultobj) {
     if (resultobj === 'no results') {
         return "<p>No results</p>";
     }
+    if (resultobj.msg){
+        return "<p>" + resultobj.msg +"</p>";
+    }
+    if (resultobj.error){
+        return "<p style='color:red'>" + resultobj.error +"</p>";
+    }
+    const start_row = " <tr>\n";
     if (resultobj.results.bindings && resultobj.results.bindings.length > 0) {
-        var result, i, j, binding, fieldtype, fieldname, nextfieldname, fielddatatype, nextfielddatatype;
-        result = "<table class='table'>\n";
-        result = result + "  <tr>\n";
-        for (i = 0; i < resultobj.head.vars.length; i += 1) {
-            fieldtype = resultobj.head.vars[i];
+        var nextfieldname, nextfielddatatype;
+        let result = "<table class='table'>\n";
+        result = result + start_row;
+        for (let i = 0; i < resultobj.head.vars.length; i += 1) {
+            const fieldtype = resultobj.head.vars[i];
             if (!uricolumn(resultobj, fieldtype)) {
                 result = result + "  <th>" + fieldtype + "</th>\n";
             }
         }
         result = result + "  </tr>\n";
-        for (j = 0; j < resultobj.results.bindings.length; j += 1) {
-            binding = resultobj.results.bindings[j];
-            result = result + "  <tr>\n";
-            for (i = 0; i < resultobj.head.vars.length; i += 1) {
-                fieldname = resultobj.head.vars[i];
-                fielddatatype = binding[fieldname].type;
-                if (i < resultobj.head.vars.length - 1) {
-                    nextfielddatatype = binding[resultobj.head.vars[i + 1]].type;
+        for (let j = 0; j < resultobj.results.bindings.length; j += 1) {
+            let binding = resultobj.results.bindings[j];
+            result = result + start_row;
+            const head = resultobj.head;
+            const vars = head.vars;
+            for (let i = 0; i < vars.length; i += 1) {
+                const fieldname = vars[i];
+                let fielddatatype = binding[fieldname].type;
+                if (i < vars.length - 1) {
+                    nextfielddatatype = binding[vars[i + 1]].type;
                 } else {
                     nextfielddatatype = '';
                 }
                 if (nextfielddatatype === 'uri') {
-                    nextfieldname = resultobj.head.vars[i + 1];
+                    nextfieldname = vars[i + 1];
                     result = result + "    <td> <a href= '" + binding[nextfieldname].value + "'>" + binding[fieldname].value + "</a></td>\n";
                 } else {
                     if (fielddatatype !== 'uri') {
@@ -53,5 +60,5 @@ var resulttable = function (resultobj) {
         return result;
     }
     return "<p>No results</p>";
-};
+}
 
