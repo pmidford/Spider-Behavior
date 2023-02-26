ee616ca8ba00a058caf76ba13b31c9dafeba7759
midford <peter.midford@gmail.com>
2023-02-26 12:54:09 -0500

    add some more myhost options.

--- src/main/webapp/static/env.js
+++ src/main/webapp/static/env.js
@@ -1,3 +1,5 @@
 //hope to find a better way to do this
-var myhost = "https://arachb.org/";
-//var myhost = "http://127.0.0.1:8080/";
+//const myhost = "https://arachb.org/";  //production
+var myhost = "http://127.0.0.1:8080/";   //local jetty
+//var myhost = "http://127.0.0.1:8080/spider-behavior/";  //local tomcat
+//var myhost = "http://arachb.org:8080/spider-behavior/"  //on board tomcat
